791da942c367a47db1ade89f812814175842b3da
asger <asger.meldgaard@gmail.com>
2024-05-05 16:31:35 +0200

    Include external libs

--- app.py
+++ app.py
@@ -10,11 +10,19 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+class EngineResources:
+    def __init__(self) -> None:
+        with open('./engine/gl_matrix.html', 'r') as f:
+            self.__matrix_libs = f.read()
+    @property
+    def external_libs(self):
+        return self.__matrix_libs;
+        
 class Engine(Resource):
     def get(self):
-        with open('./engine/gl_matrix.html', 'r') as f:
-            html = f.read()
-            return html
+        ENGINE = EngineResources()
+       
+        return ENGINE.external_libs
         
 
 api.add_resource(Engine, '/GetEngine')
