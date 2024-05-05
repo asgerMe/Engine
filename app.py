fc2e3706d82646587f16d6b4d0c8c53a442784f0
asger <asger.meldgaard@gmail.com>
2024-05-05 19:33:54 +0200

    Include external libs

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ api = Api(app)
 class EngineResources:
     def __init__(self) -> None:
         with open('./engine/gl_matrix.html', 'r') as f:
-            self.__matrix_libs = BeautifulSoup(f.read(), 'html.parser')
+            self.__matrix_libs = BeautifulSoup(f.read(), 'html.parser').prettify()
             
     @property
     def external_libs(self):
